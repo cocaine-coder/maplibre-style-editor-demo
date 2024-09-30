1c21a3345177c9ab77ebcdc5644352be305e15d3
cocaine_coder <tracywang1997@outlook.com>
2024-09-30 10:27:47 +0800

    update

--- src/components/Match.vue
+++ src/components/Match.vue
@@ -4,7 +4,7 @@
         <div v-for="v, i in matchValues" :key="i" class="match-item">
             <label v-if="v.key">{{ v.key }}</label>
             <select v-else v-model="v.key">
-                <option v-for="o in matchOptions" :key="o">{{o}}</option>
+                <option v-for="o in matchOptions" :key="o">{{ o }}</option>
             </select>
             <slot :data="v"></slot>
             <button v-if="i === 0" :disabled="matchOptions.length === 0" @click="handleAddMatchValue">+</button>
@@ -14,8 +14,9 @@
 </template>
 
 <script setup lang="ts">
-import { computed, ref, watch } from 'vue';
+import { computed, reactive, watch } from 'vue';
 import { TAllExpressionSpecificationPropertyName } from './types';
+import {isEqual} from 'es-toolkit';
 
 const props = defineProps<{
     map: maplibregl.Map,
@@ -31,21 +32,23 @@ type TMatchValue = {
     value: any
 };
 
-const matchValues = ref<TMatchValue[]>([{
+const matchValues = reactive<TMatchValue[]>([{
     key: "默认值",
     value: props.defaultValue
 }]);
 
-const matchOptions = computed(()=>{
-   const res = new Array<any>();
-   props.keys.forEach(k=>{
-        if(!matchValues.value.some(x=>x.key === k))
+const matchOptions = computed(() => {
+    const res = new Array<any>();
+    props.keys.forEach(k => {
+        if (!matchValues.some(x => x.key === k))
             res.push(k);
-   });
-   return res;
+    });
+    return res;
 });
 
-watch(matchValues, a => {
+watch(matchValues, (a, b) => {
+    console.log(isEqual(a, b));
+
     const v = props.pValue.concat(
         a.reduce((p, c, i) => {
             if (i === 0) {
@@ -60,25 +63,25 @@ watch(matchValues, a => {
     );
 
     props.map.setPaintProperty(props.layer, props.property, v.length === 3 ? v[2] : v);
-},{deep:true});
+}, {});
 
 function handleAddMatchValue() {
-    for (let i = 0; i < matchValues.value.length; i++) {
-        const matchValue = matchValues.value[i];
+    for (let i = 0; i < matchValues.length; i++) {
+        const matchValue = matchValues[i];
         if (matchValue.key && matchValue.value)
             continue;
         else
             return i;
     }
 
-    matchValues.value.push({
+    matchValues.push({
         key: "",
         value: props.defaultValue
     });
 }
 
 function handleSubMatchValue(index: number) {
-    matchValues.value.splice(index, 1);
+    matchValues.splice(index, 1);
 }
 
 
